1ea4471111c664e41faead6fcea92dfbac666a2a
Denis <mauguzun@gmail.com>
2023-03-04 17:56:13 +0200

    added db context

---
+++ src/WebApplication/Program.cs
@@ -0,0 +1,38 @@
+using DataAccess.Interfaces;
+using DataAccess.Sql;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+ConfigurationManager configuration = builder.Configuration;
+
+//Add database
+builder.Services.AddDbContext<IAppDbContext, AppDbContext>(builder =>builder.UseSqlServer(configuration["connectionString"]));
+builder.Services.AddDbContext<IAppDbContextReadonly, AppDbContextReadOnly>(builder =>builder.UseSqlServer(configuration["connectionString"]));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
