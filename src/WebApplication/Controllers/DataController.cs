bfa28d1f3f030f853ae7660f0356578fecec2281
Denis <mauguzun@gmail.com>
2023-03-30 20:29:37 +0300

    moved auth setings in webapplication

---
+++ src/WebApplication/Controllers/DataController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicine.WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Content("Index");
+        }
+
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Secret()
+        {
+            return Content("Secret");
+        }
+
+    }
+}
--- src/WebApplication/Program.cs
+++ src/WebApplication/Program.cs
@@ -1,17 +1,25 @@
+using Medicine.Auth;
 using Medicine.DataAccess.Interfaces;
 using Medicine.DataAccess.Sql;
 using Medicine.Web.UseCases.Utils;
 using Medicine.WebApplication.GraphQL.Reminder.Queries;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
+
 builder.Services.AddControllers()
         .AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -55,6 +63,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
