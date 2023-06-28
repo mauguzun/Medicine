b126fedeeef24c03894e4a61dc1a9ed8cf691f89
Denis <mauguzun@gmail.com>
2023-06-28 20:57:27 +0200

    small refactor

---
+++ src/Entities/Medicine.Entities/Models/UserMedicineWorkerLog.cs
@@ -0,0 +1,12 @@
+﻿using Medicine.Entities.Enums;
+using Medicine.Entities.Models.Base;
+
+namespace Medicine.Entities.Models
+{
+    public class UserMedicineWorkerLog : Entity
+    {
+        public int UserMedicineWorkerId;
+        public UserMedicineWorker? UserMedicineWorker;
+        public UserMedicineWorkerRelationStatus UserMedicineWorkerRelationStatus { get; set; }
+    }
+}
