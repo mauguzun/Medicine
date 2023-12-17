0c20f73e3888102bd34dbc84edffc41ef413f3d9
Denis <mauguzun@gmail.com>
2023-12-17 16:34:44 +0100

    added  arch test

---
+++ tests/ArchTest/ArchTests.cs
@@ -0,0 +1,30 @@
+using NetArchTest.Rules;
+
+namespace ArchTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var assembly = typeof(Medicine.Entities.Models.Base.Entity).Assembly;
+
+            var otherProjects =
+                new[] { "Medicine.Application" };
+
+            var result = Types.InAssembly(assembly)
+                  .ShouldNot()
+                  .HaveDependencyOnAll(
+                    otherProjects
+                  )
+                  .GetResult();
+
+            Assert.True(result.IsSuccessful);
+        }
+    }
+}
